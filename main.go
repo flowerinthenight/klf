611f8e88376ed0038e3b326283181a1f985896ca
flowerinthenight <chew@ulimit.co>
2018-04-07 03:09:13 +0900

    first working version

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -16,18 +20,27 @@ var (
 		Short: "kubectl logs follower for multiple pods",
 		Long:  `A simple wrapper for [kubectl logs] for multiple pods.`,
 	}
+
+	addprefix bool
 )
 
 func main() {
 	log.SetFlags(0)
-	rootCmd.AddCommand(TailCmd())
+	rootCmd.AddCommand(tailcmd())
 	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func TailCmd() *cobra.Command {
+func errexit(err error) {
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+}
+
+func tailcmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "tail <svc|dep> [extra-args]",
 		Short: "tail a k8s service/deployment logs",
@@ -38,46 +51,160 @@ func TailCmd() *cobra.Command {
 				os.Exit(1)
 			}
 
-			type _spec struct {
-				Selector json.RawMessage
-			}
-
-			type _svc struct {
-				Spec _spec `json:"spec"`
-			}
-
-			var target string
+			var (
+				err error
+				c   *exec.Cmd
+				out []byte
+				sel map[string]string
+				lbl string
+			)
 
 			switch args[0] {
 			case "svc":
-				c := exec.Command("kubectl", "get", "svc", args[1], "-o", "json")
-				out, _ := c.CombinedOutput()
+				type _spec struct {
+					Selector json.RawMessage
+				}
+
+				type _svc struct {
+					Spec _spec `json:"spec"`
+				}
+
+				var svc _svc
+
+				log.Printf("Retrieving service '%v' information...", args[1])
+				c = exec.Command("kubectl", "get", "svc", args[1], "-o", "json")
+				out, err = c.CombinedOutput()
+				errexit(err)
+
+				err = json.Unmarshal(out, &svc)
+				errexit(err)
+
+				err = json.Unmarshal(svc.Spec.Selector, &sel)
+				errexit(err)
 			case "dep":
-				c := exec.Command("kubectl", "get", "deployment", args[1], "-o", "json")
-				out, _ := c.CombinedOutput()
+				type _match struct {
+					Match json.RawMessage `json:"matchLabels"`
+				}
+
+				type _spec struct {
+					Selector _match `json:"selector"`
+				}
+
+				type _dep struct {
+					Spec _spec `json:"spec"`
+				}
+
+				var dep _dep
+
+				log.Printf("Retrieving deployment '%v' information...", args[1])
+				c = exec.Command("kubectl", "get", "deployment", args[1], "-o", "json")
+				out, err = c.CombinedOutput()
+				errexit(err)
+
+				err = json.Unmarshal(out, &dep)
+				errexit(err)
+
+				err = json.Unmarshal(dep.Spec.Selector.Match, &sel)
+				errexit(err)
 			default:
 				log.Println("Invalid input")
 				os.Exit(1)
 			}
 
-			var svc _svc
-			var sel map[string]string
-			var lbl string
-
-			json.Unmarshal(out, &svc)
-			json.Unmarshal(svc.Spec.Selector, &sel)
 			for k, v := range sel {
 				lbl += k + "=" + v + ","
 			}
 
-			log.Println("lbl", lbl)
-			os.Exit(0)
+			lbl = strings.TrimRight(lbl, ",")
+			log.Printf("Getting all pods with label(s) '%v'...", lbl)
 
 			c = exec.Command("kubectl", "get", "pod", "-l", strings.TrimRight(lbl, ","), "-o", "json")
-			out, _ = c.CombinedOutput()
-			log.Print(string(out))
+			out, err = c.CombinedOutput()
+			errexit(err)
+
+			type _metadata struct {
+				Name string `json:"name"`
+			}
+
+			type _items struct {
+				Metadata _metadata `json:"metadata"`
+			}
+
+			type _pod struct {
+				Items []_items `json:"items"`
+			}
+
+			var pod _pod
+
+			err = json.Unmarshal(out, &pod)
+			errexit(err)
+
+			log.Println("Pods to tail:", pod.Items)
+			log.Println("Log command for each: kubectl logs -f {pod}", strings.Join(args[2:], " "))
+			for _, p := range pod.Items {
+				logargs := make([]string, 0)
+				logargs = append(logargs, "logs")
+				logargs = append(logargs, "-f")
+				logargs = append(logargs, p.Metadata.Name)
+				for _, x := range args[2:] {
+					logargs = append(logargs, x)
+				}
+
+				prefix := p.Metadata.Name
+				lc := exec.Command("kubectl", logargs...)
+				outpipe, _ := lc.StdoutPipe()
+				errpipe, _ := lc.StderrPipe()
+				err = lc.Start()
+				errexit(err)
+
+				go func() {
+					outscan := bufio.NewScanner(outpipe)
+					for {
+						chk := outscan.Scan()
+						if !chk {
+							break
+						}
+
+						stxt := outscan.Text()
+						if addprefix {
+							log.Println("["+prefix+"]", stxt)
+						} else {
+							log.Println(stxt)
+						}
+					}
+				}()
+
+				go func() {
+					errscan := bufio.NewScanner(errpipe)
+					for {
+						chk := errscan.Scan()
+						if !chk {
+							break
+						}
+
+						stxt := errscan.Text()
+						if addprefix {
+							log.Println("["+prefix+"]", stxt)
+						} else {
+							log.Println(stxt)
+						}
+					}
+				}()
+			}
+
+			errs := make(chan error)
+
+			go func() {
+				c := make(chan os.Signal)
+				signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+				sig := fmt.Errorf("%s", <-c)
+				errs <- sig
+			}()
+
+			<-errs
 		},
 	}
 
+	cmd.Flags().BoolVar(&addprefix, "add-prefix", addprefix, "add pod name as log prefix")
 	return cmd
 }
